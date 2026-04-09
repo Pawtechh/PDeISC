50d48fd0ea84c30db97702eb1f88e4fbe385f1a4
Luis Hernan <luishernanpocorenacorvalab@gmail.com>
2026-04-09 19:49:57 -0300

    actualizacion

---
+++ 1_NodeJS/NJS1/ejercicio4.js
@@ -0,0 +1,8 @@
+import * as calc from './calculos.js'; 
+
+console.log("--- RESULTADOS EJERCICIO 4 ---");
+
+console.log("Suma (5+3):", calc.sumar(5, 3));
+console.log("Resta (8-6):", calc.restar(8, 6));
+console.log("Multiplicación (3*11):", calc.multiplicar(3, 11));
+console.log("División (30/5):", calc.dividir(30, 5));
