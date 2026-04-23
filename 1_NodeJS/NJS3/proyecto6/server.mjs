59c2dc361d2d516560ff682e7abb685ccffe1d79
Luis Hernan <luishernanpocorenacorvalab@gmail.com>
2026-04-23 20:02:20 -0300

    asdasd
    asd

---
+++ 1_NodeJS/NJS3/proyecto6/server.mjs
@@ -0,0 +1,19 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const app = express();
+const PORT = 3006;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
